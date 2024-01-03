dfce2d9cf32c1ab8c0e6fd20dfcddeff13bafb09
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-04 02:06:12 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 113.73 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args){
+        System.out.println(
+            "!@#$%^&*(\\'\"<>?:;"
+        );
+    }
+}
