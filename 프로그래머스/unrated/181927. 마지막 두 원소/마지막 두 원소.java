6117f3c0070d33687b268c33fe523c0fa9e8487d
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-09 08:55:35 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,18 +1,6 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int len = num_list.length;
-        int[] answer = new int[len+1];
-        int lastNum = 0;
-        if(num_list[len-1] > num_list[len-2]){
-            lastNum = num_list[len-1] - num_list[len-2];
-        }else{
-            lastNum = num_list[len-1]*2;
-        }
-        for(int i=0;i<len;i++){
-            answer[i] = num_list[i];
-        }
-        answer[len] = lastNum;
-        
+        int[] answer = {};
         return answer;
     }
 }
