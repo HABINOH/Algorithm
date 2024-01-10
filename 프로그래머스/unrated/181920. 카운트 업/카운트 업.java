4bf13829d59bb1ba8e155fe1a5f44c994931248b
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-10 09:20:42 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=start_num; i<=end_num; i++){
+            list.add(i);
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
