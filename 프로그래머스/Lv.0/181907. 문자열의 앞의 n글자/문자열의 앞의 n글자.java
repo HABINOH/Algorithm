642d64fd3a525b195e7665c55022bcf13f6ef22c
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-12 12:24:40 +0900

    [level Lv.0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            sb.append(my_string.charAt(i));
+        }
+        return sb.toString();
+    }
+}
