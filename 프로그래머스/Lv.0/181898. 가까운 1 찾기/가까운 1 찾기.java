8b2e0da573758328f12d5471145751057c683464
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-15 09:11:09 +0900

    [level Lv.0] Title: 가까운 1 찾기, Time: 0.11 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = Integer.MAX_VALUE;
-        
-        for(int i=idx;i<arr.length;i++){
-            if(arr[i] != 0){
-                answer = Math.min(i, answer);
-            }
-        }
-        if(answer == Integer.MAX_VALUE){
-            answer = -1;
-        }
-        
+        int answer = 0;
         return answer;
     }
 }
