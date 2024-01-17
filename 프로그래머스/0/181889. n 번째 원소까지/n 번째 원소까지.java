d64f61fcc5e5bcf5f7793538ca66977432e70623
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-17 09:04:53 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public ArrayList solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            list.add(num_list[i]);
+        }
+
+        
+        return list;
+    }
+}
