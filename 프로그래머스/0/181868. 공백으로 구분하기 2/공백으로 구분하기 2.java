c45d126b1ef41506c8d6d9f933b6c57800e1c5a0
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-19 17:58:39 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.05 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public ArrayList solution(String my_string) {
+        ArrayList<String> answer = new ArrayList<>();
+        StringTokenizer stk = new StringTokenizer(my_string, " ");
+        
+        while(stk.hasMoreTokens()){
+            answer.add(stk.nextToken());
+        }
+        
+        
+        return answer;
+    }
+}
