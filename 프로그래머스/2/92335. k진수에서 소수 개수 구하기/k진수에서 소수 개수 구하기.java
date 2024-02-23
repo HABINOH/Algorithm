81a1bd2b0a92d5bf11ade49d564226e0c9db6fe5
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-23 12:23:08 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.09 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        String convertNum = Integer.toString(n, k);
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<convertNum.length();i++){
+            if(convertNum.charAt(i) != '0'){
+                sb.append(convertNum.charAt(i));
+            }else{
+                if(sb.length()!=0 && isTrue(sb.toString())){answer++;}
+                if(sb.length()!=0)
+                    sb.delete(0, sb.length()+1);
+            }
+        }
+        if(sb.length() != 0){
+            if(isTrue(sb.toString())){answer++;}
+        }
+        return answer;
+    }
+    public boolean isTrue(String str){
+        if(str.equals("1")){return false;}
+        long num = Long.parseLong(str);
+        boolean check = true;
+        for(long i=2;i<=Math.sqrt(num);i++){
+            if(num % i == 0){check = false; break;}
+        }
+        return check;
+    }
+}
