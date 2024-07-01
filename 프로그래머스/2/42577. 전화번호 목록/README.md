acbef3a3c7c0984edc026dd0794380d0e9cfddb5
Habin <108309664+HABINOH@users.noreply.github.com>
2024-07-01 12:33:16 +0900

    [level 2] Title: 전화번호 목록, Time: 1141.08 ms, Memory: 675 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna) 
 
 ### 성능 요약
 
-메모리: 96.4 MB, 시간: 307.70 ms
+메모리: 675 MB, 시간: 1141.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 01일 12:18:16
+2024년 07월 01일 12:33:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,71 @@
 import java.util.*;
-class Solution {
-    public boolean solution(String[] phone_book) {
+class Solution{
+    public boolean solution(String[] phone_book){
         boolean answer = true;
         
         Arrays.sort(phone_book);
         
-        for(int i=1;i<phone_book.length;i++){
-            String str = phone_book[i-1];
-            if(phone_book[i].startsWith(str)){
+        Trie trie = new Trie();
+        
+        for(int i=0;i<phone_book.length;i++){
+            trie.insert(phone_book[i]);
+        }
+        
+        for(int i=0;i<phone_book.length;i++){
+            if(trie.search(phone_book[i])){
                 answer = false;
                 break;
             }
         }
         return answer;
     }
+    
+    class Node{
+        Map<Character, Node> child = new HashMap<>();
+        String str;
+        boolean end;
+    }
+    
+    class Trie{
+        Node root = new Node();
+        
+        void insert(String s){
+            Node node = this.root;
+            for(int i=0;i<s.length();i++){
+                node = node.child.computeIfAbsent(s.charAt(i), key -> new Node());
+                node.str = s;
+            }
+            node.end = true;
+        }
+        
+        boolean search(String s){
+            Node node = this.root;
+            for(int i=0;i<s.length();i++){
+                node = node.child.getOrDefault(s.charAt(i), null);   
+                if(node.str.equals(s) || node == null){
+                    return false;
+                }
+            }
+            return node.end;
+        }
+    }
 }
+
+
+// import java.util.*;
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         boolean answer = true;
+        
+//         Arrays.sort(phone_book);
+        
+//         for(int i=1;i<phone_book.length;i++){
+//             String str = phone_book[i-1];
+//             if(phone_book[i].startsWith(str)){
+//                 answer = false;
+//                 break;
+//             }
+//         }
+//         return answer;
+//     }
+// }
