d916f1e812ce3a0db9c786dcc41dc342e54d3443
Habin <108309664+HABINOH@users.noreply.github.com>
2024-07-18 14:06:09 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1804 ms, Memory: 128104 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int m;
+    static int n;
+    static long[] snack;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(stk.nextToken());
+        n = Integer.parseInt(stk.nextToken());
+
+        snack = new long[n];
+        stk = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            snack[i] = Long.parseLong(stk.nextToken());
+        }
+        Arrays.sort(snack);
+        long start = 1;
+        long end = Long.MAX_VALUE-1;
+        long result = 0;
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+            if(check(mid)){
+                start = mid + 1;
+                result = mid;
+            }else{
+                end = mid - 1;
+            }
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static boolean check(long mid){
+        long sum = 0;
+        for(int i=0;i<n;i++){
+            sum += snack[i] / mid;
+        }
+        return sum >= m;
+    }
+}
