70b7c4f4aa1f1ccba7681736ad45fa8465d521aa
Habin <108309664+HABINOH@users.noreply.github.com>
2024-07-28 15:29:58 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,15 @@
+-- 코드를 작성해주세요
+
+SELECT
+ ID
+,EMAIL
+,FIRST_NAME
+,LAST_NAME
+FROM
+ DEVELOPER_INFOS 
+WHERE
+   SKILL_1 IN ('Python') 
+OR SKILL_2 IN ('Python')
+OR SKILL_3 IN ('Python')
+ORDER BY
+ 1
