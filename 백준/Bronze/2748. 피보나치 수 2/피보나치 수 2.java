91fe107a36ca233258621a25f83e21811a2d7218
Habin <108309664+HABINOH@users.noreply.github.com>
2024-09-06 19:53:44 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static long[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        dp = new long[n+1];
+        if(n>=1){
+            dp[1] = 1;
+        }
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
