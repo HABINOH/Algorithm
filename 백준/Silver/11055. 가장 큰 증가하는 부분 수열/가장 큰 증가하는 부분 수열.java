cc7c2410e295f05448c74c2a7e3e671a77c4a64c
Habin <108309664+HABINOH@users.noreply.github.com>
2024-09-08 21:53:59 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 88 ms, Memory: 12148 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dp;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        arr = new int[n];
+        stk = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int result = 0;
+        dp = new int[n+1];
+        for(int i=0;i<n;i++){
+            dp[i] = arr[i];
+            for(int j=0;j<i;j++){
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
