de567e8e7958ad80d77921c76cd45a07032ef1d0
Habin <108309664+HABINOH@users.noreply.github.com>
2024-09-08 16:17:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 72 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+
+        dp = new int[1001];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = (dp[i-1] + (dp[i-2] * 2)) % 10007;
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
