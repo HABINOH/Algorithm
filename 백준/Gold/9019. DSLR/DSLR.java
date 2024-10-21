38404ab157213ebcd5e9e0e553a6beeb1a0b8bf6
Habin <108309664+HABINOH@users.noreply.github.com>
2024-10-21 20:09:39 +0900

    [Gold IV] Title: DSLR, Time: 6076 ms, Memory: 310612 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -8,51 +8,64 @@ public class Main {
         StringTokenizer stk = new StringTokenizer(br.readLine());
 
         int tc = Integer.parseInt(stk.nextToken());
-        for(int testCase=0; testCase < tc; testCase++){
+        for (int testCase = 0; testCase < tc; testCase++) {
             stk = new StringTokenizer(br.readLine());
             int startNum = Integer.parseInt(stk.nextToken());
             int endNum = Integer.parseInt(stk.nextToken());
 
-            boolean[] visited = new boolean[10001];
+            // 배열 크기 줄임
+            boolean[] visited = new boolean[10000];
             Queue<Integer> queue = new LinkedList<>();
-            String[] cmd = new String[10001];
+            String[] cmd = new String[10000];  // 10001에서 10000으로 줄임
 
             visited[startNum] = true;
             queue.add(startNum);
             Arrays.fill(cmd, "");
 
-            while(!queue.isEmpty() && !visited[endNum]){
+            while (!queue.isEmpty()) {
                 int curNum = queue.poll();
 
+                // 목표 도달 시 바로 탈출
+                if (curNum == endNum) {
+                    bw.write(cmd[curNum]);
+                    bw.newLine();
+                    break;
+                }
+
+                // 각 명령어에 따른 상태 계산
                 int D = (2 * curNum) % 10000;
                 int S = curNum == 0 ? 9999 : curNum - 1;
                 int L = (curNum % 1000) * 10 + curNum / 1000;
                 int R = (curNum % 10) * 1000 + curNum / 10;
 
-
-                if(!visited[D]){
+                // D 연산
+                if (!visited[D]) {
                     queue.add(D);
                     visited[D] = true;
                     cmd[D] = cmd[curNum] + "D";
                 }
-                if(!visited[S]){
+
+                // S 연산
+                if (!visited[S]) {
                     queue.add(S);
                     visited[S] = true;
                     cmd[S] = cmd[curNum] + "S";
                 }
-                if(!visited[L]){
+
+                // L 연산
+                if (!visited[L]) {
                     queue.add(L);
                     visited[L] = true;
                     cmd[L] = cmd[curNum] + "L";
                 }
-                if(!visited[R]){
+
+                // R 연산
+                if (!visited[R]) {
                     queue.add(R);
                     visited[R] = true;
                     cmd[R] = cmd[curNum] + "R";
                 }
             }
-            bw.write(cmd[endNum]);
-            bw.newLine();
         }
 
         bw.flush();
