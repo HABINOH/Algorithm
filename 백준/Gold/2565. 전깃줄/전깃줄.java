5700b32ebd5257237c460d59a99af08c6e52f032
Habin <108309664+HABINOH@users.noreply.github.com>
2024-10-23 11:14:41 +0900

    [Gold V] Title: 전깃줄, Time: 68 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,47 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] wire;
-    static int[] dp;
+    static int[][] wires;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer stk = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(stk.nextToken());
-
-        wire = new int[n][2];
-        dp = new int[n];
-
+        wires = new int[n][2];
         for(int i=0;i<n;i++){
             stk = new StringTokenizer(br.readLine());
-            wire[i][0] = Integer.parseInt(stk.nextToken());
-            wire[i][1] = Integer.parseInt(stk.nextToken());
+            wires[i][0] = Integer.parseInt(stk.nextToken());
+            wires[i][1] = Integer.parseInt(stk.nextToken());
         }
 
-        Arrays.sort(wire, new Comparator<int[]>() {
+        Arrays.sort(wires, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[0] - o2[0];
             }
         });
 
+        int[] dp = new int[n];
         int max = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             dp[i] = 1;
             for(int j=0; j<i; j++){
-                if(wire[i][1] > wire[j][1]){
+                if(wires[i][1] > wires[j][1]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
             max = Math.max(max, dp[i]);
         }
 
-        bw.write(String.valueOf(n - max));
+        bw.write(String.valueOf(n-max));
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
