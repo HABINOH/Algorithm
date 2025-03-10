393a31f48a11588edb085147a704c106955b0e7e
Habin <108309664+HABINOH@users.noreply.github.com>
2025-03-10 22:10:14 +0900

    [Silver III] Title: 합 구하기, Time: 544 ms, Memory: 56508 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk;
+        int n = Integer.parseInt(br.readLine());
+        stk = new StringTokenizer(br.readLine());
+        int[] prefixSum = new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            prefixSum[i] = prefixSum[i-1] + Integer.parseInt(stk.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for(int i=0;i<m;i++){
+            stk = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(stk.nextToken());
+            int to = Integer.parseInt(stk.nextToken());
+
+            bw.write(String.valueOf(prefixSum[to] - prefixSum[from-1]));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
