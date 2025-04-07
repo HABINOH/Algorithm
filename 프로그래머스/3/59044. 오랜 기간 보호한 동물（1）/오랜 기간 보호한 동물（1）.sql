cd07e1bb412d8dc74f612d0d16357d23b5c4c372
Habin <108309664+HABINOH@users.noreply.github.com>
2025-04-07 23:50:29 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,19 +1,51 @@
--- 코드를 입력하세요
 SELECT 
-  NAME
-, DATETIME
-FROM
- ANIMAL_INS ins 
-WHERE ins.animal_id NOT IN (
-     SELECT 
-      ANIMAL_ID
-     FROM 
-      ANIMAL_OUTS outs
-     WHERE 
-      ins.animal_id = outs.animal_id
-)
-ORDER BY
- 2 LIMIT 3
+      A.NAME, 
+      A.DATETIME 
+FROM 
+      ANIMAL_INS A
+LEFT JOIN 
+      ANIMAL_OUTS B
+ON 
+      A.ANIMAL_ID = B.ANIMAL_ID
+WHERE 
+      B.ANIMAL_ID IS NULL
+ORDER BY 
+      A.DATETIME LIMIT 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 코드를 입력하세요
+# SELECT 
+#   NAME
+# , DATETIME
+# FROM
+#  ANIMAL_INS ins 
+# WHERE ins.animal_id NOT IN (
+#      SELECT 
+#       ANIMAL_ID
+#      FROM 
+#       ANIMAL_OUTS outs
+#      WHERE 
+#       ins.animal_id = outs.animal_id
+# )
+# ORDER BY
+#  2 LIMIT 3
 
 
 
