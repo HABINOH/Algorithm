896b500df8aa77f4d7b8d5ba9065ef470fb303c6
Habin <108309664+HABINOH@users.noreply.github.com>
2025-04-13 12:12:12 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,20 +1,44 @@
 -- 코드를 입력하세요
+SELECT T1.ID, T1.NAME, T1.HOST_ID
+FROM PLACES T1
+JOIN (
+SELECT HOST_ID FROM PLACES A
+GROUP BY A.HOST_ID
+HAVING COUNT(*) > 1
+) T2
+ON T1.HOST_ID = T2.HOST_ID
+ORDER BY T1.ID
 
-WITH HOST AS (
-SELECT *
-FROM
- PLACES
-GROUP BY HOST_ID
-HAVING COUNT(HOST_ID) >= 2
-ORDER BY ID
-)
-SELECT
-  p.id
-, p.name
-, p.host_id
-FROM PLACES p
-JOIN HOST h
-ON p.host_id = h.host_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# WITH HOST AS (
+# SELECT *
+# FROM
+#  PLACES
+# GROUP BY HOST_ID
+# HAVING COUNT(HOST_ID) >= 2
+# ORDER BY ID
+# )
+# SELECT
+#   p.id
+# , p.name
+# , p.host_id
+# FROM PLACES p
+# JOIN HOST h
+# ON p.host_id = h.host_id
 
 
 
